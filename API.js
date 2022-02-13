8ebb47a830d63a5575174b30a409d8a5d508eaef
karan Bhati <bhatikaran467@gmail.com>
2022-02-13 10:53:18 +0530

    changes

--- API.js
+++ API.js
@@ -1,84 +1,22 @@
-const express = require("express");
+const http = require("http");
+var url = require("url");
 const axios = require("axios");
-const router = express.Router();
-
-const app = express();
-
-router.get("/page1/:sitechanger/:params", (req, res) => {
+const server = http.createServer((req, res) => {
   const headers = {
     "Cache-Control": "no-cache",
     host: "extramovies.town",
     Accept: "*/*",
   };
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
+  var q = url.parse(req.url, true).query;
+  var txt = q.url;
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
   axios
-    .get(
-      `https://extramovies.${req.params.sitechanger}/?s=${req.params.params}`,
-      headers
-    )
+    .post(`https://suzihaza.com/api/source/${txt}`, headers)
     .then((response) => {
-      const ab = response.data;
-      // console.log(response.data);
-      res.send(ab);
+      res.end(JSON.stringify(response.data));
     });
 });
-
-router.get("/page2/s=:param2", (req, res) => {
-  const headers = {
-    "Cache-Control": "no-cache",
-    host: "extramovies.town",
-    Accept: "*/*",
-  };
-  const decoded = decodeURIComponent(req.params.param2);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  axios.get(decoded, headers).then((response) => {
-    const ab = response.data;
-    // console.log(response.data);
-    res.send(ab);
-  });
-});
-router.get("/page3/url=:param3", (req, res) => {
-  const headers = {
-    "Cache-Control": "no-cache",
-    host: "extramovies.town",
-    Accept: "*/*",
-  };
-  console.log(req.params.param3);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  axios
-    .post(`https://suzihaza.com/api/source/${req.params.param3}`, headers)
-    .then((response) => {
-      const ab = response.data;
-      res.send(ab);
-    });
-});
-
-app.use("/.netlify/functions/api", router);
-module.exports.handler = serverless(app);
+server.listen(5000);
--- index.js
+++ index.js
@@ -1,84 +1,22 @@
-const express = require("express");
+const http = require("http");
+var url = require("url");
 const axios = require("axios");
-const app = express();
-
-app.get("/page1/:sitechanger/:params", (req, res) => {
+const server = http.createServer((req, res) => {
   const headers = {
     "Cache-Control": "no-cache",
     host: "extramovies.town",
     Accept: "*/*",
   };
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
+  var q = url.parse(req.url, true).query;
+  var txt = q.url;
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
   axios
-    .get(
-      `https://extramovies.${req.params.sitechanger}/?s=${req.params.params}`,
-      headers
-    )
+    .post(`https://suzihaza.com/api/source/${txt}`, headers)
     .then((response) => {
-      const ab = response.data;
-      // console.log(response.data);
-      res.send(ab);
+      res.end(JSON.stringify(response.data));
     });
 });
-
-app.get("/page2/s=:param2", (req, res) => {
-  const headers = {
-    "Cache-Control": "no-cache",
-    host: "extramovies.town",
-    Accept: "*/*",
-  };
-  const decoded = decodeURIComponent(req.params.param2);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  axios.get(decoded, headers).then((response) => {
-    const ab = response.data;
-    // console.log(response.data);
-    res.send(ab);
-  });
-});
-app.get("/page3/url=:param3", (req, res) => {
-  const headers = {
-    "Cache-Control": "no-cache",
-    host: "extramovies.town",
-    Accept: "*/*",
-  };
-  console.log(req.params.param3);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.header("origin") ||
-      req.header("x-forwarded-host") ||
-      req.header("referer") ||
-      req.header("host")
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  axios
-    .post(`https://suzihaza.com/api/source/${req.params.param3}`, headers)
-    .then((response) => {
-      const ab = response.data;
-      res.send(ab);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.warn(`App listening on http://localhost:${PORT}`);
-});
+server.listen(5000);
